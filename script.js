cb554f439d4c497ed94ecb99d396f868cba132a5
Pelayo Méndez <1971236+pelayomendez@users.noreply.github.com>
2026-03-06 20:33:23 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('.item-button');
+
+button.addEventListener('click', function () {
+  console.log('Add to cart clicked!');
+  // Add your cart functionality here
+  alert('Item added to cart!');
+});
